1730e3185c0b98ad8b0b12ecc7c74bb13025e8de
Yeray Dominguez <yeraydguez@gmail.com>
2014-04-10 23:22:39 +0100

    Entidad compania creada

---
+++ companias/src/YDD/GTBundle/Entity/Compania.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace YDD\GTBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Compania
+ *
+ * @ORM\Table(name="compania", uniqueConstraints={@ORM\UniqueConstraint(name="compania", columns={"nombre"})})
+ * @ORM\Entity
+ */
+class Compania
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="smallint", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=20, nullable=false)
+     */
+    private $nombre;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return Compania
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+}
